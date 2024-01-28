9c69398072cf83ccd5a95819503acd2d9f049d9a
NotYou404 <dominik.reinartz@hotmail.com>
2024-01-28 18:41:56 +0100

    Reworked some of the scraper functions

--- grubengeraet/extractor/scraper.py
+++ grubengeraet/extractor/scraper.py
@@ -13,7 +13,7 @@ from typing import Optional
 import bs4
 import dateparser
 import pandas as pd
-import regex as re
+import regex as re  # See rules_reworked()
 
 from .emojis import is_emoji
 
@@ -24,10 +24,15 @@ WHITESPACES = r"\xad͏\u061cᅟᅠ឴឵\u180e\u2000\u2001\u2002\u2003\u2004\u20
               r"\u206f\u3000\ufeff"\
               r"\ufe0f\ufe0f"\
 
+# Source: https://de.m.wikipedia.org/wiki/Satzzeichen
+PUNCTUATION = r".?!\"„“‚‘»«‹›,;:'’–—‐\-·/\()\[\]<>{}…☞‽¡¿⸘、"
+
 COLUMNS = [
+    "post_id",
     "post_num",
     "page_num",
     "author",
+    "author_id",
     "creation_datetime",
     "content",
     "like_count",
@@ -159,6 +164,7 @@ def construct_dataframe(
         )
 
         for message in find_all_messages(soup):
+            post_id = get_post_id(message)
             post_num = get_post_num(message)
             if postrange:
                 if post_num not in postrange:
@@ -172,6 +178,7 @@ def construct_dataframe(
             content_tag = find_message_content(message)  # Can be modified
 
             author = unmodified_message["data-author"]
+            author_id = get_author_id(unmodified_message)
             creation_datetime = get_message_creation_time(unmodified_message)
             is_edited = has_edited_message(unmodified_message)
 
@@ -180,7 +187,7 @@ def construct_dataframe(
 
             spoiler_count = get_amount_of_spoilers(unmodified_message)
 
-            mentioned_list = get_list_of_mentioned_usernames(
+            mentioned_list = get_list_of_mentioned_ids(
                 unmodified_message
             )
             mentions_count = len(mentioned_list)
@@ -199,7 +206,7 @@ def construct_dataframe(
 
             word_count = get_amount_of_words(content_tag)
 
-            rules_compliance_check_result = rules_reworked(content)
+            rules_compliance_check_result = rules_reworked(content, word_count)
             rulebreak_reasons = [
                 k for k, v in rules_compliance_check_result.items() if not v
             ]
@@ -208,9 +215,11 @@ def construct_dataframe(
             message_series = pd.Series(
                 data=(
                     [
+                        post_id,
                         post_num,
                         page_num,
                         author,
+                        author_id,
                         creation_datetime,
                         content,
                         like_count,
@@ -262,6 +271,43 @@ def get_post_num(message: bs4.element.Tag) -> int:
     """
     postnum_str = message.find_all("li")[3].get_text(strip=True)[1:]
     return int(postnum_str.replace(".", ""))
+
+
+def get_post_id(message: bs4.element.Tag) -> int:
+    """
+    Get the post id of a message.
+
+    :param message: The message's element tag object.
+    :type message: bs4.element.Tag
+    :return: The post id.
+    :rtype: int
+    """
+    match = re.search(r"post-(\d+)", message["data-content"])  # type: re.Match
+    if not match:
+        raise ValueError("Post does not have an ID.")
+    return int(match.group(1))
+
+
+def get_author_id(message: bs4.element.Tag) -> str:
+    """
+    Get the author id as string.
+
+    :param message: The message's element tag object.
+    :type message: bs4.element.Tag
+    :return: The author's id
+    :rtype: str
+    """
+    try:
+        # Pings look the same, however the author comes first
+        return message.find("a", class_="username")["data-user-id"]
+    except TypeError:
+        # Deleted Member has no ID
+        return 0
+    except KeyError:
+        # First, a non-existent user that doesn't have the anchor tag at all
+        # Second, that user pinged someone, that ping anchor is corrupt.
+        # Corrupted Tag that doesn't have the data-user-id param. Ex.: https://uwmc.de/p102857  # noqa
+        return 0
 
 
 def get_amount_of_likes(message: bs4.element.Tag) -> int:
@@ -380,7 +426,7 @@ def get_amount_of_spoilers(message: bs4.element.Tag) -> int:
     return len(message.find_all("div", class_="bbCodeSpoiler"))
 
 
-def get_list_of_mentioned_usernames(message: bs4.element.Tag) -> list[str]:
+def get_list_of_mentioned_ids(message: bs4.element.Tag) -> list[str]:
     """Retrieves a list of mentioned usernames. Get the amount of mentions
     by using len() on the list.
 
@@ -390,15 +436,15 @@ def get_list_of_mentioned_usernames(message: bs4.element.Tag) -> list[str]:
     Returns:
         list[str]: The list containing all usernames.
     """
-    usernames: list[str] = []
-    for mention in message.find_all("a", class_="username"):
-        if (uname := mention.get_text(strip=True))[0] == "@":
-            # There can also be other anchor tags with that class.
-            # However, only mentions start with @.
-            # Perf. note: str[0]=="@" performs almost 3 times faster than
-            # str.startswith("@").
-            usernames.append(uname[1:])
-    return usernames
+    ids: list[str] = []
+    for mention in message.find_all("a", class_="username")[1:]:
+        # Ignore the first one, it's the post author
+        try:
+            ids.append(mention["data-user-id"])
+        except KeyError:
+            # Corrupt Mention Tag (ex.: https://uwmc.de/p90996)
+            ids.append(0)
+    return ids
 
 
 def get_amount_of_words(content: bs4.element.Tag) -> int:
@@ -436,7 +482,7 @@ def get_mapping_of_emojis_and_frequency(
 
 def _count_words(string_: str) -> int:
     """Counts the amount of words in a string by utilizing the
-    str.split() method. Splits on any whitespace character and
+    re.split() method. Splits on any whitespace character and
     discards empty strings.
 
     Args:
@@ -445,7 +491,11 @@ def _count_words(string_: str) -> int:
     Returns:
         int: The amount of words in the string.
     """
-    return len(re.split(rf"[\s{string.punctuation}]", string_))
+    # Should split this into 2: `two-worded`
+    split = re.split(rf"[\s{PUNCTUATION}]", string_)
+    return len(
+        [i for i in split if re.search(r"\w", i)]
+    )
 
 
 def has_edited_message(message: bs4.element.Tag) -> bool:
@@ -479,6 +529,10 @@ def check_rules_compliance(
         with the reasons (str) in case it's not compliant. Reasons can
         be "word_count", "first_letter" or "punctuation".
     """
+    raise DeprecationWarning(
+        "check_rules_compliance() is deprecated, please use rules_reworked() "
+        "instead."
+    )
     # Rules:
     # - At least 5 words (word_count)
     # - First letter must be capitalized (first_letter)
@@ -539,18 +593,23 @@ def get_message_creation_time(message: bs4.element.Tag) -> dt.datetime:
     return dateparser.parse(iso_string)  # type: ignore
 
 
-def _find_first_letter_index(string_: str):
-    for char in string_:
-        if char in string.ascii_letters:
-            return string_.index(char)
-    return None
+def rules_reworked(content: str, word_count: int) -> dict[str, bool]:
+    """
+    Checks if a post is compliant to the rules defined for WALA.
 
+    Rules:
+        - At least 5 words (word_count)
+        - First letter must be capitalized (first_letter)
+        - Trailing punctuation (punctuation)
 
-# Source: https://de.m.wikipedia.org/wiki/Satzzeichen
-PUNCTUATION = r".?!\"„“‚‘»«‹›,;:'’–—‐\-·/\()\[\]<>{}…☞‽¡¿⸘、"
-
-
-def rules_reworked(content: str) -> dict[str, bool]:
+    :param content: The message content
+    :type content: str
+    :param word_count: Amount of words in the content
+    :type word_count: int
+    :return: Dictionary mapping criterion to wether it complies. Criteria are:
+    word_count, first_letter, punctuation
+    :rtype: dict[str, bool]
+    """
     content = content.strip()  # Remove trailing and leading whitespaces
     content = content.strip(WHITESPACES)  # Better list
 
@@ -560,18 +619,12 @@ def rules_reworked(content: str) -> dict[str, bool]:
         "punctuation": True,
     }
 
-    if not content:
+    if not content:  # Empty message
         return {
             k: False for k in compliance.keys()
         }
 
     # Word count
-    # Using re.split() instead of str.split() as re supports all whitespaces
-    # out of the box.
-    word_count = len(re.split(
-        fr"[\s{PUNCTUATION}]",  # Split words by whitespace and punctuation
-        content,
-    ))
     if word_count < 5:
         compliance["word_count"] = False
 
@@ -582,8 +635,8 @@ def rules_reworked(content: str) -> dict[str, bool]:
     # This works with all Unicode letters, including äöü, ß and âáà.
     first_letter_match = re.search('\\p{L}', content, re.UNICODE)
     if (
-        first_letter_match is None  # No letter in content
-        or not first_letter_match.captures()[0].isupper()  # noqa  # letter not uppercase
+        first_letter_match is None or  # No letter in content
+        not first_letter_match.captures()[0].isupper()  # letter not uppercase
     ):
         compliance["first_letter"] = False
 
@@ -591,8 +644,8 @@ def rules_reworked(content: str) -> dict[str, bool]:
     # Last forum emoji has already been replaced with a dot
     last_char = content[-1]
     if (
-        last_char not in PUNCTUATION
-        and not is_emoji(last_char)  # noqa
+        last_char not in PUNCTUATION and
+        not is_emoji(last_char)
     ):
         compliance["punctuation"] = False
 
